0412c15ac1fd9fa836273380444b8592da00c0c7
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-12-01 19:36:17 +0800

    Create d1.py

---
+++ Python/d1.py
@@ -0,0 +1,7 @@
+s = ... # input
+# Part 1
+print(max(eval(s.replace(*'\n+').replace('++',","))))
+# Part 2
+print(sum(sorted(eval(s.replace(*'\n+').replace('++',",")))[-3:]))
+# Both parts (prints in order of part 2, part 1)
+print(sum(v:=sorted(eval(open(0).read().replace(*'\n+').replace('++',",")))[-3:]),v[2])
