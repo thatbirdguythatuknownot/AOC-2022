3b47c624e6a05ccb808f5b0679b14cf3b56fd39d
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-12-01 19:42:37 +0800

    use `s` instead of `open(0).read`

--- Python/d1.py
+++ Python/d1.py
@@ -4,4 +4,4 @@ print(max(eval(s.replace(*'\n+').replace('++',","))))
 # Part 2
 print(sum(sorted(eval(s.replace(*'\n+').replace('++',",")))[-3:]))
 # Both parts (prints in order of part 2, part 1)
-print(sum(v:=sorted(eval(open(0).read().replace(*'\n+').replace('++',",")))[-3:]),v[2])
+print(sum(v:=sorted(eval(s.replace(*'\n+').replace('++',",")))[-3:]),v[2])
