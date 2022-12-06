229511504f25939c45a2986ed715c95705db2e57
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-12-06 16:38:12 +0800

    Update d5.py

--- Python/d5.py
+++ Python/d5.py
@@ -1,6 +1,6 @@
 s=... # input
 # Both parts
-g,j,f=str.split,''.join,s.split('\n')
+g,j=str.split,''.join;f=g(s,'\n')
 for o in-1,1:
  d=[' ']+[j(g(j(m)))for m in zip(*f[:8])][1::4]
  for l in f[10:]:a,b,c=map(int,g(l)[1::2]);d[c]=d[b][:a][::o]+d[c];d[b]=d[b][a:]
