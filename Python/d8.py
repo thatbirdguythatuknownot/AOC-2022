17894ddd0094c97d1810925507d92cc183ec0e9f
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-12-08 18:40:17 +0800

    Update d8.py

--- Python/d8.py
+++ Python/d8.py
@@ -5,9 +5,9 @@ for _ in range(9801):t=l[i:=_//99];s+=t[j:=_%99]>min(eval("max(%ror'/'),"*4%(u:=
 print(s)
 # Part 2
 l=*open(0),;r=*zip(*l),;c=[]
-for _ in range(9801):t=l[i:=_//99];j=_%99;v,w,x,y=(a+(a<b)for a,b in zip(eval("(z:=1)*sum(z:=z*t[j]>x for x in%r),"*4%(u:=(t[j+1:-1],t[:j][::-1],r[j][i+1:],r[j][:i][::-1]))),map(len,u)));c+=v*w*x*y,
-print(max(c))
+for _ in range(9801):t=l[i:=_//99];j=_%99;v,w,x,y=(a+(a<b)for a,b in zip(eval("(z:=1)*sum(z:=z*t[j]>x for x in%r),"*4%(u:=(t[j+1:-1],t[:j][::-1],r[j][i+1:],r[j][:i][::-1]))),map(len,u)));c=max(c,v*w*x*y)
+print(c)
 # Both parts
 l=*open(s:=0),;r=*zip(*l),;c=[]
-for _ in range(9801):t=l[i:=_//99];s+=t[j:=_%99]>min(eval("max(%ror'/'),"*4%(u:=(t[j+1:-1],t[:j][::-1],r[j][i+1:],r[j][:i][::-1]))));v,w,x,y=(a+(a<b)for a,b in zip(eval("(z:=1)*sum(z:=z*t[j]>x for x in%r),"*4%u),map(len,u)));c+=v*w*x*y,
-print(s,max(c))
+for _ in range(9801):t=l[i:=_//99];s+=t[j:=_%99]>min(eval("max(%ror'/'),"*4%(u:=(t[j+1:-1],t[:j][::-1],r[j][i+1:],r[j][:i][::-1]))));v,w,x,y=(a+(a<b)for a,b in zip(eval("(z:=1)*sum(z:=z*t[j]>x for x in%r),"*4%u),map(len,u)));c=max(c,v*w*x*y)
+print(s,c)
