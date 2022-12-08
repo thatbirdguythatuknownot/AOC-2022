a16c7e0d5964ae272caf2effa3aaac1819710059
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-12-08 18:09:08 +0800

    Update d8.py

--- Python/d8.py
+++ Python/d8.py
@@ -1,17 +1,13 @@
 s=... # input
 # Part 1
-l=s.split('\n');r=*zip(*l),;s=0
-for _ in range(9801):i=_//99;j=_%99;t=l[i];s+=min(eval("max(%ror'/'),"*4%(u:=(t[j+1:-1],t[:j][::-1],r[j][i+1:],r[j][:i][::-1]))))<t[j]
+l=*open(s:=0),;r=*zip(*l),
+for _ in range(9801):t=l[i:=_//99];s+=t[j:=_%99]>min(eval("max(%ror'/'),"*4%(u:=(t[j+1:-1],t[:j][::-1],r[j][i+1:],r[j][:i][::-1]))))
 print(s)
 # Part 2
-l=s.split('\n');r=*zip(*l),;e=0
-for _ in range(9801):
- i=_//99;j=_%99;t=l[i];v,w,x,y=eval("(z:=1)and sum(z and(x<t[j]or(z:=0))for x in%r),"*4%(u:=(t[j+1:-1],t[:j][::-1],r[j][i+1:],r[j][:i][::-1])));m,n,o,p=map(len,u);v+=v<m;w+=w<n;x+=x<o;y+=y<p;c=v*w*x*y
- if c>e:e=c
-print(e)
+l=*open(0),;r=*zip(*l),;c=[]
+for _ in range(9801):t=l[i:=_//99];j=_%99;v,w,x,y=(a+(a<b)for a,b in zip(eval("(z:=1)*sum(z:=z*t[j]>x for x in%r),"*4%(u:=(t[j+1:-1],t[:j][::-1],r[j][i+1:],r[j][:i][::-1]))),map(len,u)));c+=v*w*x*y,
+print(max(c))
 # Both parts
-l=*open(0),;r=*zip(*l),;e=s=0
-for _ in range(9801):
- i=_//99;j=_%99;t=l[i];s+=min(eval("max(%ror'/'),"*4%(u:=(t[j+1:-1],t[:j][::-1],r[j][i+1:],r[j][:i][::-1]))))<t[j];v,w,x,y=eval("(z:=1)and sum(z and(x<t[j]or(z:=0))for x in%r),"*4%u);m,n,o,p=map(len,u);v+=v<m;w+=w<n;x+=x<o;y+=y<p;c=v*w*x*y
- if c>e:e=c
-print(s,e)
+l=*open(s:=0),;r=*zip(*l),;c=[]
+for _ in range(9801):t=l[i:=_//99];s+=t[j:=_%99]>min(eval("max(%ror'/'),"*4%(u:=(t[j+1:-1],t[:j][::-1],r[j][i+1:],r[j][:i][::-1]))));v,w,x,y=(a+(a<b)for a,b in zip(eval("(z:=1)*sum(z:=z*t[j]>x for x in%r),"*4%u),map(len,u)));c+=v*w*x*y,
+print(s,max(c))
